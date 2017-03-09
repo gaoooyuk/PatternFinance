fae5e4c13b8e9f0a4175af94a0f2d18c32149b1e
gaoooyuk <chuckgao.cg@gmail.com>
2017-03-09 14:38:01 +0800

    qrcode

---
+++ tools/qrcode/g.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# python myqr.py https://www.patternfinance.com/article/shb365_oupaiyishu -n shb365_oupaiyishu.png
+# python myqr.py https://www.patternfinance.com/article/chaikin_money_flow_cmf -n chaikin_money_flow_cmf.png
+# python myqr.py https://www.patternfinance.com/article/ashares_shengsichang -n ashares_shengsichang.png
+# python myqr.py https://www.patternfinance.com/article/luhan_qinghanjijin -n luhan_qinghanjijin.png
+python myqr.py https://www.patternfinance.com/article/touzigushi_sky -n touzigushi_sky.png
+# python myqr.py https://www.patternfinance.com/article/touzigushi_2 -n touzigushi_2.png
+# python myqr.py https://www.patternfinance.com/article/tencent_weihuangjin -n tencent_weihuangjin.png
+# python myqr.py https://www.patternfinance.com/article/blackstone_story -n blackstone_story.png
+# python myqr.py https://www.patternfinance.com/article/renquan_starvc -n renquan_starvc.png
+# python myqr.py https://www.patternfinance.com/article/touzigushi_1 -n touzigushi_1.png
+# python myqr.py https://www.patternfinance.com/article/mean_reversion_part_3 -n mean_reversion_part_3.png
+# python myqr.py https://www.patternfinance.com/article/mean_reversion_part_2 -n mean_reversion_part_2.png
+# python myqr.py https://www.patternfinance.com/article/volume_by_price -n volume_by_price.png
+# python myqr.py https://www.patternfinance.com/article/stock_market_anomalies -n stock_market_anomalies.png
+# python myqr.py https://www.patternfinance.com/article/mean_reversion_part_1 -n mean_reversion_part_1.png
+# python myqr.py https://www.patternfinance.com/article/id -n id.png
+# python myqr.py https://www.patternfinance.com/article/id -n id.png
+# python myqr.py https://www.patternfinance.com/article/id -n id.png
+# python myqr.py https://www.patternfinance.com/article/id -n id.png
+# python myqr.py https://www.patternfinance.com/article/id -n id.png
