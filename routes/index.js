beb34d943a6b2c05bcf42bf2cb2327af7ba29951
gaoooyuk <chuckgao.cg@gmail.com>
2017-03-19 11:50:26 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -10,19 +10,66 @@ router.get('/', function(req, res, next) {
 	res.sendFile(path.join(home_dir + '/main.html'));
 });
 
+router.get('/about', function(req, res, next) {
+	var home_dir = path.join(global.dirRoot, 'qml/')
+
+	res.render('about', 
+	{ 
+		"html_description": "",
+		"html_keywords": "关于我们,磨石金融,新媒体,财经,内容平台"
+	})
+});
+
+router.get('/books', function(req, res, next) {
+	var home_dir = path.join(global.dirRoot, 'qml/')
+
+	res.render('books', 
+	{ 
+		"html_description": "",
+		"html_keywords": "Books,磨石金融,宽客"
+	})
+});
+
+router.get('/company', function(req, res, next) {
+	var home_dir = path.join(global.dirRoot, 'qml/')
+
+	res.render('channel', 
+	{ 
+		"channel": "金融帝国",
+		"channel_qml": "website_company",
+		"html_description": "",
+		"html_keywords": "金融帝国,磨石金融,新媒体,金融巨鳄"
+	})
+});
+
+router.get('/dashboard', function(req, res, next) {
+	var home_dir = path.join(global.dirRoot, 'qml/')
+	res.sendFile(path.join(home_dir + '/dashboard.html'));
+});
+
+router.get('/edge', function(req, res, next) {
+	var home_dir = path.join(global.dirRoot, 'qml/')
+
+	res.render('edge', 
+	{ 
+		"html_description": "磨石金融Edge频道为您收录世界上最前沿的量化交易研究和金融科技创新。",
+		"html_keywords": "前沿,Edge,磨石金融,宽客,量化交易,学术研究,量化对冲基金,Quant,Quatitative,Hedge Fund,Strategy"
+	})
+});
+
+router.get('/joinus', function(req, res, next) {
+	var home_dir = path.join(global.dirRoot, 'qml/')
+
+	res.render('joinus', 
+	{ 
+		"html_description": "",
+		"html_keywords": "加入我们,加入磨石,磨石金融,新媒体,内容平台,磨石工具"
+	})
+});
+
 router.get('/login', function(req, res, next) {
 	var home_dir = path.join(global.dirRoot, 'qml/')
 	res.sendFile(path.join(home_dir + '/login.html'));
-});
-
-router.get('/partners', function(req, res, next) {
-	var home_dir = path.join(global.dirRoot, 'qml/')
-
-	res.render('partners', 
-	{ 
-		"html_description": "",
-		"html_keywords": "合作伙伴,Gimletech,技术联盟,媒体联盟"
-	})
 });
 
 router.get('/liqi', function(req, res, next) {
@@ -41,27 +88,26 @@ router.get('/mindstore', function(req, res, next) {
 	res.render('mindstore', 
 	{ 
 		"html_description": "在Mindstore频道发现最近都有哪些不容错过的讲座和会议。",
-		"html_keywords": "Mindstore,讲座,会议,磨石金融,学术会议,Conference,Seminar,Talk,Pitch"
+		"html_keywords": "Mindstore,讲座,会议,磨石金融,学术会议,Workshop,Conference,Seminar,Talk,Pitch"
 	})
 });
 
-router.get('/edge', function(req, res, next) {
+router.get('/my', function(req, res, next) {
 	var home_dir = path.join(global.dirRoot, 'qml/')
-
-	res.render('edge', 
-	{ 
-		"html_description": "磨石金融Edge频道为您收录世界上最前沿的量化交易研究和金融科技创新。",
-		"html_keywords": "前沿,Edge,磨石金融,宽客,量化交易,学术研究,量化对冲基金,Quant,Quatitative,Hedge Fund,Strategy"
-	})
-});
-
-router.get('/about', function(req, res, next) {
-	var home_dir = path.join(global.dirRoot, 'qml/')
-
-	res.render('about', 
+	res.render('my', 
 	{ 
 		"html_description": "",
-		"html_keywords": "关于我们,磨石金融,新媒体,财经,内容平台"
+		"html_keywords": "磨石金融"
+	})
+});
+
+router.get('/partners', function(req, res, next) {
+	var home_dir = path.join(global.dirRoot, 'qml/')
+
+	res.render('partners', 
+	{ 
+		"html_description": "",
+		"html_keywords": "合作伙伴,Gimletech,技术联盟,媒体联盟"
 	})
 });
 
@@ -101,42 +147,9 @@ router.get('/story', function(req, res, next) {
 	})
 });
 
-router.get('/company', function(req, res, next) {
-	var home_dir = path.join(global.dirRoot, 'qml/')
-
-	res.render('channel', 
-	{ 
-		"channel": "金融帝国",
-		"channel_qml": "website_company",
-		"html_description": "",
-		"html_keywords": "金融帝国,磨石金融,新媒体,金融巨鳄"
-	})
-});
-
-router.get('/joinus', function(req, res, next) {
-	var home_dir = path.join(global.dirRoot, 'qml/')
-
-	res.render('joinus', 
-	{ 
-		"html_description": "",
-		"html_keywords": "加入我们,加入磨石,磨石金融,新媒体,内容平台,磨石工具"
-	})
-});
-
-router.get('/dashboard', function(req, res, next) {
-	var home_dir = path.join(global.dirRoot, 'qml/')
-	res.sendFile(path.join(home_dir + '/dashboard.html'));
-});
-
-router.get('/my', function(req, res, next) {
-	var home_dir = path.join(global.dirRoot, 'qml/')
-	res.render('my', 
-	{ 
-		"html_description": "",
-		"html_keywords": "磨石金融"
-	})
-});
-
+// 
+// 
+// 
 router.get('/article/*', function(req, res, next) {
 	var home_dir = path.join(global.dirRoot, 'qml/')
 	var fn = req.params['0']
