ab9be284c477b3e3c71a443c8af79afb1986fd68
gaoooyuk <chuckgao.cg@gmail.com>
2017-05-09 16:16:38 +0800

    fix

--- Automation/launch_live.sh
+++ Automation/launch_live.sh
@@ -3,7 +3,9 @@
 pm2 delete --silent seekingalpha
 pm2 delete --silent marketwatch
 pm2 delete --silent thestreet
+pm2 delete --silent cnbc
 
 pm2 start live_seekingalpha.js --name="seekingalpha"
 pm2 start live_marketwatch.js --name="marketwatch"
 pm2 start live_thestreet.js --name="thestreet"
+pm2 start live_cnbc.js --name="cnbc"
